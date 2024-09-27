236be270e07fc29dfcb13c6e85b8b057f9a12382
Vinay@itworks <vinay.poul@infotech.works>
2024-09-27 11:45:02 +0530

    Created database model as Doctor, Patient, Appointment and established relationship between them.

--- db/migrate/20240927054627_create_patients.rb
+++ db/migrate/20240927054627_create_patients.rb
@@ -1,8 +1,8 @@
 class CreatePatients < ActiveRecord::Migration[7.2]
   def change
     create_table :patients do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.integer :age
       t.string :gender
       t.string :contact
